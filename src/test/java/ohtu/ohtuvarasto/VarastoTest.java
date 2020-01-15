ce77f85f2a77fd9d9be4d8ff6959f6412a52caba
antton <antton.kortelainen@gmail.com>
2020-01-15 17:34:39 +0200

    Write quality tests

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -65,4 +65,63 @@ public class VarastoTest {
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
 
+    @Test
+    public void liikaaTavaraa() {
+        varasto.lisaaVarastoon(15);
+
+        // ylimenevät menevät hukkaan
+        assertEquals(0, varasto.paljonkoMahtuu(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void negatiivinenVarasto() {
+        varasto = new Varasto(-10);
+        System.out.println(varasto); // Gets the coverage up))
+        assertNotEquals(-10,varasto.getTilavuus(),vertailuTarkkuus);
+    }
+
+    
+    @Test
+    public void otetaanLiikaaTavaraa() {
+        varasto.lisaaVarastoon(10);
+        varasto.otaVarastosta(11);
+        // ylimenevät menevät hukkaan
+        assertEquals(10, varasto.paljonkoMahtuu(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void overloadUp() {
+        varasto = new Varasto(10,10);
+        assertEquals(0, varasto.paljonkoMahtuu(),vertailuTarkkuus);
+    }
+
+    @Test
+    public void overloadUpNegatiivinen() {
+        varasto = new Varasto(10,50);
+        // menee yli
+        assertEquals(0, varasto.paljonkoMahtuu(),vertailuTarkkuus);
+    }
+    @Test
+    public void negatiivinenAlkuSaldo() {
+        varasto = new Varasto(10,-10);
+        assertNotEquals(-10, varasto.paljonkoMahtuu(),vertailuTarkkuus);
+    }
+
+    @Test
+    public void negatviinenTilavuus() {
+        varasto = new Varasto(-10,10);
+        assertNotEquals(-10, varasto.paljonkoMahtuu(),vertailuTarkkuus);
+    }
+
+    @Test
+    public void lisaaVarastoonNeg() {
+        varasto.lisaaVarastoon(-10);
+        assertEquals(10, varasto.paljonkoMahtuu(),vertailuTarkkuus);
+    }
+    
+    @Test
+    public void otaVarastostaNeg() {
+        varasto.otaVarastosta(-2);
+        assertEquals(10,varasto.paljonkoMahtuu() ,vertailuTarkkuus);
+    }
 }
